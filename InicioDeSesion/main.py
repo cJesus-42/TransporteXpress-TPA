351eead9d12261ea127f371d329d03054da85c05
millasayenn <millagodoyh@gmail.com>
2025-04-21 19:42:11 -0400

    Add files via upload

--- InicioDeSesion/main.py
+++ InicioDeSesion/main.py
@@ -3,14 +3,19 @@ from tkinter import *
 from tkinter import ttk as ttk
 from usuarios import Usuario
 
-
 class InicioSesionApp:
     def __init__(self, root):
         self.root = root #ventana principal
         self.root.title("TransporteXpress/Inicio de Sesión")
-        self.root.geometry("800x400")
-        self.root.minsize(width=800, height=400)
-        self.root.maxsize(width=800, height=400)
+        
+        #Centrar Ventana
+        self.anchoVentana=1200
+        self.altoVentana=600
+        self.anchoPantalla = self.root.winfo_screenwidth()
+        self.altoPantalla = self.root.winfo_screenheight()
+        self.x = (self.anchoPantalla-self.anchoVentana) // 2
+        self.y = (self.altoPantalla-self.altoVentana) // 2
+        self.root.geometry(f"{self.anchoVentana}x{self.altoVentana}+{self.x}+{self.y-70}")
         
         #Usuario de Prueba
         self.usuario1= Usuario ("admin", "1234")
@@ -24,16 +29,15 @@ class InicioSesionApp:
         #Fondo
         self.imagen = PhotoImage(file="C:/Users/Millaray/Desktop/SEMESTRE 3/Proyecto TPA/Imagenes/camionbg.png")
         imagenLabel = Label(self.root, image=self.imagen)
-        imagenLabel.place(x=-2, y=0)
+        imagenLabel.pack(side=LEFT, fill="both", expand=TRUE)
         
         #Contenedor
-        contenedor = Frame(self.root, bg="orange")
-        #contenedor.pack()
-        contenedor.place(x=400, y=100)
+        contenedor = Frame(self.root, bg="orange", )
+        contenedor.place(relx=0.5, rely=0.5, anchor=S)
         
         #textos y títulos
-        titulo = Label(contenedor, text="TransporteXpress", font=("Montserrat SemiBold", 20), bg="orange")
-        titulo.grid(column=0, row=0, padx=10, pady=10, columnspan=2)
+        titulo = Label(contenedor, text="TransporteXpress",font=("Montserrat SemiBold", 20), bg="orange")
+        titulo.grid(column=0, row=0, ipadx=20, padx=10, pady=10, columnspan=2)
         
         nombreLabel = Label(contenedor, text="Nombre: ", font=("Montserrat", 14), bg="orange")
         nombreLabel.grid(column=0, row=1)
@@ -42,11 +46,9 @@ class InicioSesionApp:
         contraseniaLabel.grid(column=0, row=2)
         
         #Entradas de textos
-        #nombreUsuario = StringVar()
         nombreEntry = Entry(contenedor, textvariable=self.nombreUsuario, bg="#faedcd")
         nombreEntry.grid(column=1, row=1)
         
-        #contraseniaUsuario = StringVar()
         contraseniaEntry = Entry(contenedor, textvariable=self.contraseniaUsuario, bg="#faedcd", show="*")
         contraseniaEntry.grid(column=1, row=2)
         
@@ -56,6 +58,7 @@ class InicioSesionApp:
         
         registrarButton = ttk.Button(contenedor, text="Registrar", command=self.registrarUsuario)
         registrarButton.grid(column=0, row=3, ipadx=5, ipady=5,padx=10, pady=10)        
+        
         
     def iniciarSesion(self):
         nombre = self.nombreUsuario.get()
@@ -71,3 +74,5 @@ if __name__=="__main__":
     root = Tk()
     app = InicioSesionApp(root)
     root.mainloop()
+
+####### HASTA AQUÍ LA MILLY Y COMIENZA LA SOFI ########
---
+++ README.txt
@@ -0,0 +1,29 @@
+Para las clases iniciar con mayúscula (ej: InicioDeSesion()),
+y para las funciones empezar con minúscula y luego mayúsculas (ej: def crearCamion())
+*Con JSON y Tkinter
+
+Registro:
+    *Solo para los nuevos usuarios
+
+En Inicio de Sesión:
+    *Borrar contraseña después de 3 intentos y ingresar nueva contraseña
+    *Crear, modificar, editar y eliminar Usuarios.
+
+Después de Iniciar Sesión:
+1. Usuario:
+    - Ver que camiones estan disponibles
+    - Arrendar camiones junto con el chofer.
+2. Administrador:
+    - Crear camiones, modificarlos, editarlos y eliminarlos
+        *Los camiones deben tener seguro, permiso de circulación, patente (en JSON)
+    - Crear choferes, modificarlos, editarlos y eliminarlos
+    - Enviar camiones a taller
+
+En la 1era Presentación:
+    - Carta Gantt (fecha de inicio hasta el 2 de mayo)
+    - Indicar que cosas queremos agregarle al proyecto a futuro (para el próximo avance)
+
+Inicio de Sesion: Millaray
+Página Principal de Administrador: Sofía
+Hacer clases de camiones: Cristian *Buscar como conectar datos en JSON
+Hacer clases de choferes: Paulina
